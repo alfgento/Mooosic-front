b00fa29430d4fc9879b75fa77a47a656fe17e7c9
Ángel Fernández <alfgento@gmail.com>
2024-04-26 12:30:08 +0200

    first push

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import './Home.css'
+
+function Home () {
+    return (
+        <>
+            <img className='logo' src="../assets/vaca.jpg" />
+            <h1>MOOOSIC</h1>
+        </>
+    )
+}
+export default Home;
